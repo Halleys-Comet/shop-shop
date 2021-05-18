391a683ce219d5e6f89beaff11d3775d523069bb
Halleys-Comet <jhalley230@gmail.com>
2021-05-17 19:00:44 -0500

    added useDispatch and useSelector

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+import {useSelector, useDispatch} from 'react-redux';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
 
   const {
     image,
@@ -15,6 +14,9 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
+
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { cart } = state
 
